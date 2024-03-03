1579937e35ed926fa2dd31511ae82b56d91a5223
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-03-03 14:23:30 +0100

    add: mongoDB & sidebar & mobilebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Roboto } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
+import { ClerkProvider } from "@clerk/nextjs"
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -20,10 +21,18 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("roboto antialiased", roboto.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "#624cf5",
+        },
+      }}
+    >
+      <html lang="en">
+        <body className={cn("roboto antialiased", roboto.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
