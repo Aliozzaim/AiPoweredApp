68c9ff833cec33e1d268d70e10088d6491913519
Ali Ozzaim <125793435+Aliozzaim@users.noreply.github.com>
2024-03-14 12:13:33 +0100

    fix :sideBar layout

--- components/shared/SideBar.tsx
+++ components/shared/SideBar.tsx
@@ -11,7 +11,7 @@ const Sidebar = () => {
   const pathname = usePathname()
 
   return (
-    <aside className="sidebar">
+    <aside className="sidebar +">
       <div className="flex size-full flex-col gap-4">
         <Link href="/" className="sidebar-logo">
           <Image
